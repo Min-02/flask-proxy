2c086be3c5a6a50e018fadd4955db6c85d18092b
Min-02 <kyung05060@gmail.com>
2025-05-26 17:26:06 +0900

    Update proxy.py

--- proxy.py
+++ proxy.py
@@ -396,7 +396,7 @@ def predicted_sales():
 @app.route("/api/population_chart", methods=["GET"])
 def population_chart():
     # 한글 폰트 설정
-    font_path = 'NanumGothic-Regular.ttf'  # 서버 환경에 맞게 변경하세요
+    font_path = 'nanumgothic-regular.ttf'  # 서버 환경에 맞게 변경하세요
     font_prop = fm.FontProperties(fname=font_path).get_name()
     plt.rc('font', family=font_prop)
     plt.rcParams['axes.unicode_minus'] = False  # 마이너스 깨짐 방지
